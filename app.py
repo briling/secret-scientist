75ef0b5e55613d8486150df63a687713837a7782
Ksenia <briling@mail.ru>
2024-03-10 03:04:28 +0100

    Keep the score

--- app.py
+++ app.py
@@ -4,30 +4,27 @@ from PIL import Image, ImageTk
 import numpy as np
 
 
-SCORE = 100   # how many people * 3
-
-
 class ImageRow:
-    def __init__(self, master, data, label_text=None, data_dir='data', padding=(4,4)):
+    def __init__(self, master, data, SCORE,
+                 label_text=None, data_dir='data', padding=(4,4)):
         self.frame = tk.Frame(master, bd=8, relief=tk.RIDGE)
         self.frame.pack(side=tk.TOP, padx=8*padding[0], pady=8*padding[1])
         if label_text is not None:
             self.label = tk.Label(self.frame, text=label_text, fg="black", font=tkFont.Font(size=16))
             self.label.pack()
-        self.image_widgets = [ImageWidget(self.frame, f'{data_dir}/{path}', name.replace(' \\n ','\n'))
+        self.image_widgets = [ImageWidget(self.frame, f'{data_dir}/{path}', name.replace(' \\n ','\n'), SCORE)
                               for (name, path) in data]
 
 
 class ImageWidget:
-    def __init__(self, master, image_path, label_text,
-                 image_0_path='data/moon.png',
-                 image_size=(128,128),
-                 padding=(4,4)):
+    def __init__(self, master, image_path, label_text, SCORE,
+                 image_0_path='data/moon.png', image_size=(128,128), padding=(4,4)):
 
         self.photo_0 = ImageTk.PhotoImage(Image.open(image_0_path).resize(image_size))
         self.photo   = ImageTk.PhotoImage(Image.open(image_path).resize(image_size))
         self.label_text = label_text
         self.widget_state = 0
+        self.score = SCORE
 
         frame = tk.Frame(master)
         frame.pack(side=tk.LEFT, padx=padding[0], pady=padding[1])
@@ -40,25 +37,25 @@ class ImageWidget:
 
 
     def on_click(self, event):
-        global SCORE
         if self.widget_state<=1:
             if self.widget_state==0:
                 self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo)
             else:
                 self.label.config(text=self.label_text)
             self.widget_state += 1
-            SCORE -= 1; print(f'{SCORE=}')
+            self.score.set(self.score.get() - 1)
 
 
 class PeopleChoice:
-    def __init__(self, master, image_paths,
+    def __init__(self, master, image_paths, SCORE,
                  image_size=(128,128), padding=(4,4),
-                 switch_pic='data/switch_camera.png'):
+                 switch_pics=('data/switch_camera.png', 'data/switch_camera_red.png')):
 
         self.photos = [ImageTk.PhotoImage(Image.open(path).resize(image_size)) for path in image_paths]
-        self.switch_pic = ImageTk.PhotoImage(Image.open(switch_pic).resize(image_size))
+        self.switch_pics = [ImageTk.PhotoImage(Image.open(path).resize(image_size)) for path in switch_pics]
         self.widget_state = 0
         self.first_switch = 1
+        self.score = SCORE
 
         def put_photo(master, photo):
             frame = tk.Frame(master)
@@ -72,7 +69,7 @@ class PeopleChoice:
         self.frame = tk.Frame(master)
         self.frame.pack(side=tk.TOP, padx=16*padding[0], pady=16*padding[1])
         self.canvas1 = put_photo(self.frame, self.photos[0])
-        put_photo(self.frame, self.switch_pic)
+        self.canvas_switch = put_photo(self.frame, self.switch_pics[0])
         self.canvas2 = put_photo(self.frame, self.photos[1])
 
 
@@ -82,8 +79,23 @@ class PeopleChoice:
         self.widget_state = (self.widget_state+1)%2
         if self.first_switch:
             self.first_switch = 0
+            self.canvas_switch.create_image(0, 0, anchor=tk.NW, image=self.switch_pics[1])
         else:
-            global SCORE; SCORE -= 1; print(f'{SCORE=}')
+            self.score.set(self.score.get() - 1)
+
+
+class GameControls:
+    def __init__(self, master, SCORE, TOTAL, padding=(4,4)):
+        self.frame = tk.Frame(master)
+        self.frame.pack(side=tk.TOP, padx=16*padding[0], pady=16*padding[1])
+        label1 = tk.Label(self.frame, text='SCORE:', fg="black", font=tkFont.Font(size=28))
+        self.label_score = tk.Label(self.frame, textvariable=SCORE, fg="black", font=tkFont.Font(size=32))
+        label2 = tk.Label(self.frame, text='TOTAL:', fg="black", font=tkFont.Font(size=28))
+        self.label_total = tk.Label(self.frame, textvariable=TOTAL, fg="black", font=tkFont.Font(size=32))
+        label1.pack()
+        self.label_score.pack()
+        label2.pack()
+        self.label_total.pack()
 
 
 def main():
@@ -93,21 +105,23 @@ def main():
 
     root = tk.Tk()
 
+    SCORE = tk.IntVar(value=sum(len(x) for x in data_chunks)*3)
+    TOTAL = tk.IntVar(value=0)
+
     frame1 = tk.Frame(root)
     frame1.pack(side=tk.LEFT)
-    PeopleChoice(frame1, ('data/clock.png', 'data/moon.png'))
+    PeopleChoice(frame1, ('data/clock.png', 'data/moon.png'), SCORE)
 
     frame2 = tk.Frame(root)
     frame2.pack(side=tk.LEFT)
     for i, chunk in enumerate(data_chunks):
-        frame = ImageRow(frame2, chunk)
+        frame = ImageRow(frame2, chunk, SCORE)
 
-
-
+    frame3 = tk.Frame(root)
+    frame3.pack(side=tk.LEFT)
+    GameControls(frame3, SCORE, TOTAL)
 
     root.mainloop()
-
-    print(f'{SCORE=}')
 
 
 if __name__ == "__main__":
